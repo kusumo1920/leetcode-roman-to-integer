7abd66cc367fe7c6cb6cfb528b6b770e600a0edf
Kesit Budi Kusumo <kesit@kernel.id>
2024-05-23 23:14:19 +0700

    feat: define better second solution
    O(n)

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	input := "MCMXCIV"
-	output := romanToIntSolution1(input)
+	output := romanToIntSolution2(input)
 	fmt.Println(output)
 }
 
@@ -39,6 +39,33 @@ func romanToIntSolution1(s string) int {
 	if previousChar != "" {
 		result += romanMap[previousChar]
 		previousChar = ""
+	}
+
+	return result
+}
+
+func romanToIntSolution2(s string) int {
+	romanMap := map[rune]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+
+	result := 0
+	i := 0
+	for i < len(s) {
+		// subtractive case
+		if i+1 < len(s) && romanMap[rune(s[i])] < romanMap[rune(s[i+1])] {
+			result += romanMap[rune(s[i+1])] - romanMap[rune(s[i])]
+			i += 2
+		} else {
+			result += romanMap[rune(s[i])]
+			i += 1
+		}
 	}
 
 	return result
